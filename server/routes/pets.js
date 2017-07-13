a7186723bcb3305a5d7fdbae41ced10af53a8343
Amanda Kirchner <geeky.brewster@gmail.com>
2017-07-13 11:43:26 -0500

    All routes have queryText and variables from client. Testing required now.

--- server/routes/pets.js
+++ server/routes/pets.js
@@ -12,8 +12,10 @@ router.get('/', function(req, res){
       console.log('Error connecting to the database.');
       res.sendStatus(500);
     } else {
-      // **** NEED DATABASE QUERY HERE ****
-      var queryText = '"pets"."color", "visits"."check_in_status" FROM owners JOIN pets ON "owners"."id" = "pets"."owner_id" JOIN visits ON "pets"."owner_id" = "visits"."pets_id";';
+
+      var queryText = '"pets"."color", "visits"."check_in_status" FROM owners' +
+      ' JOIN pets ON "owners"."id" = "pets"."owner_id"' +
+      ' JOIN visits ON "pets"."owner_id" = "visits"."pets_id";';
       db.query(queryText, function(errorMakingQuery, result){
         done();
         if(errorMakingQuery) {
@@ -25,7 +27,6 @@ router.get('/', function(req, res){
           res.send({pets: result.rows});
           // ***** DOUBLE CHECK ORDER OF ITEMS IN THE OBJECT *****
           // owner, name, breed, color
-          // ***** FIX FOR CLIENT-SIDE IF NECESSARY *****
         }
       }); // end query
     } // end if
@@ -39,7 +40,7 @@ router.get('/getCustomers', function(req, res){
       console.log('Error connecting to the database.');
       res.sendStatus(500);
     } else {
-      // **** NEED DATABASE QUERY HERE ****
+
       var queryText = 'SELECT ("last_name", "first_name") FROM owners;';
       db.query(queryText, function(errorMakingQuery, result){
         done();
@@ -63,13 +64,13 @@ router.post('/newPet', function(req, res){
       console.log('Error connecting to the database.');
       res.sendStatus(500);
     } else {
-      // **** NEED VARIABLES FROM CLIENT-SIDE ****
+
       var pet = req.body;
       var ownerID = pet.owner;  //Need the owner ID -- NOT name here
       var name = pet.name;
       var breed = pet.breed;
       var color = pet.color;
-      // **** NEED DATABASE QUERY TEXT ****
+
       // adding a new pet, but we must make sure we link owner_id
       var queryText = 'INSERT INTO pets ("name", "breed", "color", "owner_id" ) VALUES ($1, $2, $3, $4);';
       db.query(queryText,[name, breed, color, owner], function(errorMakingQuery, result){
@@ -94,11 +95,11 @@ router.post('/newCustomer', function(req, res){
       console.log('Error connecting to the database.');
       res.sendStatus(500);
     } else {
-      // **** NEED VARIABLES FROM CLIENT-SIDE ****
+
       var customer = req.body;
       var first_name = customer.first_name;
       var last_name = customer.last_name;
-      // **** NEED DATABASE QUERY TEXT ****
+
       var queryText = "INSERT INTO owners ('first_name', 'last_name') VAULES ($1, $2);";
       db.query(queryText,[first_name, last_name], function(errorMakingQuery, result){
         done();
@@ -115,7 +116,7 @@ router.post('/newCustomer', function(req, res){
   }); // end pool
 }); // end of POST
 
-// DELETE pet from DB
+// DELETE pet from DB (Deleting pet from visits table and pets table)
 router.delete('/:id', function(req, res){
   pool.connect(function(errorConnectingToDatabase, db, done){
     if(errorConnectingToDatabase) {
@@ -123,9 +124,49 @@ router.delete('/:id', function(req, res){
       res.sendStatus(500);
     } else {
       var id = req.params.id;
-      // **** NEED DATABASE QUERY TEXT ****
-      var queryText = '';
+      // DELETE PET FROM THE VISITS TABLE
+      var queryText = 'DELETE FROM visits WHERE "pets_id" = $1;';
       db.query(queryText,[id], function(errorMakingQuery, result){
+        if(errorMakingQuery) {
+          console.log('Attempted to query with', queryText);
+          console.log('Error making query');
+          res.sendStatus(500);
+        } else {
+          // THEN DELETE PET FROM THE PETS TABLE
+          queryText = 'DELETE FROM pets WHERE "id" = $1;';
+          db.query(queryText,[id], function(errorMakingQuery, result){
+            done();
+            if(errorMakingQuery) {
+              console.log('Attempted to query with', queryText);
+              console.log('Error making query');
+              res.sendStatus(500);
+            } else {
+                // console.log(queryText);
+              res.send({pets: result.rows});
+              }
+            }); // end of second db query
+          } // end of second if else
+        }); // end of first db query
+      } // end first if else
+  }); // end pool
+}); // end of DELETE
+
+// Update pet in DB
+router.put('/', function(req, res){
+  pool.connect(function(errorConnectingToDatabase, db, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to the database.');
+      res.sendStatus(500);
+    } else {
+
+      var ownerId = req.params.id;
+      var updatedPet = req.body;
+      var name = updatedPet.name;
+      var breed = updatedPet.breed;
+      var color = updatedPet.color;
+
+      var queryText = 'UPDATE pets SET ("name", "breed", "color") = VALUES($1, $2, $3) WHERE "owner_id" = $4;';
+      db.query(queryText,[name, breed, color, ownerId], function(errorMakingQuery, result){
         done();
         if(errorMakingQuery) {
           console.log('Attempted to query with', queryText);
@@ -138,7 +179,7 @@ router.delete('/:id', function(req, res){
       }); // end query
     } // end if
   }); // end pool
-}); // end of DELETE
+}); // end of PUT
 
 // Mark pet as CHECKED IN completed in DB
 router.put('/checkin/:id', function(req, res){
@@ -148,8 +189,8 @@ router.put('/checkin/:id', function(req, res){
       res.sendStatus(500);
     } else {
       var id = req.params.id;
-      // **** NEED DATABASE QUERY HERE ****
-      var queryText = ' ';
+
+      var queryText = 'UPDATE visits SET "check_out_status" = TRUE WHERE "pets_id" = $1;';
       db.query(queryText,[id], function(errorMakingQuery, result){
         done();
         if(errorMakingQuery) {
@@ -173,8 +214,8 @@ router.put('/checkout/:id', function(req, res){
       res.sendStatus(500);
     } else {
       var id = req.params.id;
-      // **** NEED DATABASE QUERY HERE ****
-      var queryText = ' ';
+
+      var queryText = 'UPDATE visits SET "check_in_status" = FALSE WHERE "pets_id" = $1;';
       db.query(queryText,[id], function(errorMakingQuery, result){
         done();
         if(errorMakingQuery) {
@@ -190,35 +231,6 @@ router.put('/checkout/:id', function(req, res){
   }); // end pool
 }); // end of PUT
 
-// Update pet in DB
-router.put('/', function(req, res){
-  pool.connect(function(errorConnectingToDatabase, db, done){
-    if(errorConnectingToDatabase) {
-      console.log('Error connecting to the database.');
-      res.sendStatus(500);
-    } else {
-      // **** NEED VARIABLES FROM CLIENT-SIDE ****
-      var id = req.params.id;
-      var updatedPet = req.body;
-      var name = updatedPet.name;
-      var breed = updatedPet.breed;
-      var color = updatedPet.color;
 
-      // **** NEED DATABASE QUERY HERE ****
-      var queryText = 'UPDATE pets SET ("name", "breed", "color") = VALUES($1, $2, $3) WHERE "owner_id" = $4;';
-      db.query(queryText,[name, breed, color, id], function(errorMakingQuery, result){
-        done();
-        if(errorMakingQuery) {
-          console.log('Attempted to query with', queryText);
-          console.log('Error making query');
-          res.sendStatus(500);
-        } else {
-            // console.log(queryText);
-          res.send({pets: result.rows});
-        }
-      }); // end query
-    } // end if
-  }); // end pool
-}); // end of PUT
 
 module.exports = router;
